8ad501fe3d923f5d3e51aea60b8c3c93a136d6c3
Takayuki Uchida <uchida.takayuki@tis.co.jp>
2024-06-14 12:18:44 +0900

    java.jms.Queueをjakarta.jms.Queueに変更

--- src/main/java/nablarch/fw/messaging/provider/JndiLookingUpJmsMessagingProvider.java
+++ src/main/java/nablarch/fw/messaging/provider/JndiLookingUpJmsMessagingProvider.java
@@ -35,7 +35,7 @@ import java.util.Map.Entry;
  *   <!-- jakarta.jms.ConnectionFactory実装クラスをルックアップするためのJNDI名 -->
  *   <property name="connectionFactoryJndiName" value="jakarta.jms.QueueConnectionFactory"/>
  *
- *   <!-- FWで使用するキュー論理名と、java.jms.Queue実装クラスをルックアップするためのJNDI名のペア -->
+ *   <!-- FWで使用するキュー論理名と、jakarta.jms.Queue実装クラスをルックアップするためのJNDI名のペア -->
  *   <property name="destinationNamePairs">
  *     <map>
  *       <!-- key=キュー論理名、value=キューJNDI名 -->
